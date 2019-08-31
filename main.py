aee16112459bbb9a598d3124cba52370d0d6b92f
Wonkadude <darius.wigglesworth@gmail.com>
2019-08-31 19:04:04 -0400

    Changed logic for excluding gifs

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ from time import sleep
 from PIL import Image
 import subprocess
 
-sleepDuration = 15
-dailyCats = [] #array that holds all 24 dicts of cat pictures for the day 
-goodUrl1 = 'i.imgur'
-goodUrl2 = 'i.redd'     
+sleepDuration = 600
+dailyCats = [] #array that holds all 24 dicts of cat pictures for the day
+exclude1 = 'gif'
+exclude2 = 'gfycat'
+exclude3 = 'v.redd'     
 
 while True:
     #Sends REST GET request to reddit to get the desired .json file and convert it to a dict to parse
@@ -25,19 +26,15 @@ while True:
     data = response.json()
     print('The HTTP response code was ', response)
 
-    with open('data.txt', 'w') as outfile:      #Creates a file of the dict from json that it is using as data
-        json.dump(data, outfile)
-
     #Loops through dict from reddit, sees if required parameters are met
     for i in data['data']['children']:
         #if requirements are met then it creates a temp dict with needed information and adds it to daily dict
         if i.get('data').get('over_18') == False and i.get('data').get('is_video') == False:
-            if goodUrl1 in i.get('data').get('url') or goodUrl2 in i.get('data').get('url'):
+            if exclude1 in i.get('data').get('url') or exclude2 in i.get('data').get('url') or exclude3 in i.get('data').get('url'):
+                #do nothing 
+            else:
                 dailyCats.append({'title':i.get('data').get('title'), 'url':i.get('data').get('url')}) #adds needed data into a dict, adding the dict to the daily array for later use
                 #print(dailyCats) #prints array for debugging purposes
-
-    with open('output.txt', 'w') as outfile:    #Create and outfile for easier debugging
-        json.dump(dailyCats, outfile)
 
     print('dict created and appended')
     
@@ -53,3 +50,9 @@ while True:
                     sleep(sleepDuration)   #Sleep 30s for testing purposes
                     p.terminate() #kill browser
         i += 1
+
+    with open('data.txt', 'w') as outfile:      #Creates a file of the dict from json that it is using as data
+        json.dump(data, outfile)
+
+    with open('output.txt', 'w') as outfile:    #Create and outfile for easier debugging
+        json.dump(dailyCats, outfile)
